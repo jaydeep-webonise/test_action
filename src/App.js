199b1a855ccf0359e5b5737996300ef9c8e4d0e2
jaydeep <jaydeep.vishwakarma@weboniselab.com>
2021-06-21 22:37:44 +0530

    sdf

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editdfvdfvdf <code>src/App.js</code> and save to reload.
+          Editdfvdfvdf <code>src/App.js</code> and save to reload.dfgdfgdfgdfgdf
         </p>
         <a
           className="App-link"
